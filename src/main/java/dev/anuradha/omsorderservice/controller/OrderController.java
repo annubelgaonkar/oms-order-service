25b1740bb3a6c428aba7e0b83387cedb9ae29519
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2026-02-27 20:33:23 +0530

    implemented sqs publisher

--- src/main/java/dev/anuradha/omsorderservice/controller/OrderController.java
+++ src/main/java/dev/anuradha/omsorderservice/controller/OrderController.java
@@ -2,13 +2,13 @@ package dev.anuradha.omsorderservice.controller;
 
 import dev.anuradha.omsorderservice.dto.CreateOrderRequest;
 import dev.anuradha.omsorderservice.model.Order;
+import dev.anuradha.omsorderservice.repository.OrderRepository;
 import dev.anuradha.omsorderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -16,10 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderRepository orderRepository;
 
     @PostMapping
     public ResponseEntity<Order> create(
             @RequestBody CreateOrderRequest orderRequest){
         return ResponseEntity.ok().body(orderService.createOrder(orderRequest));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrder(@PathVariable UUID id){
+        return ResponseEntity.ok(
+                orderRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Order not found"))
+        );
     }
 }
